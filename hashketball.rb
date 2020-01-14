22f052f43a4b0a5c8773cfc98ab80570608c73d8
Christopher Wagstaffe <cjwagstaffe@gmail.com>
2020-01-14 21:51:20 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -156,7 +156,7 @@ def player_numbers(t_name)
     if h_a[1][:team_name] === t_name
       new = []
       h_a.each do |inner|
-        pp inner[0]
+        pp inner[1]
         #inner.each do |keys|
           #new << keys[:number]
         #end
