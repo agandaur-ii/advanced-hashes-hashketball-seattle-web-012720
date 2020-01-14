599b9fd9ee674a8b48c5af725e6a4253a52481f3
Christopher Wagstaffe <cjwagstaffe@gmail.com>
2020-01-14 22:13:16 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -169,7 +169,7 @@ def player_stats(player_name)
     array_of_all_players.each do |index| 
     index.each do |key|
       if key.include?(player_name)
-        return index
+        puts index
       end
     end    
   end
