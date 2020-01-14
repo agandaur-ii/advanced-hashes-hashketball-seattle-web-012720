d838fbff16e4b394b9b7cfdfe64a92da3350885d
Christopher Wagstaffe <cjwagstaffe@gmail.com>
2020-01-14 20:53:49 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -115,10 +115,10 @@ end
 def num_points_scored(player_name)
   array_of_all_players.each do |x| 
     if x.include?(player_name)
-      return 
+      return x[:points]
     end
-  
   end
+  puts "Not found"
 end
 
 pp array_of_all_players[0][:player_name].include?("Alan Anderson")
