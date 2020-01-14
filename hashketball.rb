a46ba22dc1702b7de9e989f8e9194a68e8101a9b
Christopher Wagstaffe <cjwagstaffe@gmail.com>
2020-01-14 22:58:47 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -181,9 +181,8 @@ def big_shoe_rebounds
   array_of_all_players.each do |index| 
     index.each do |key|
       key.max_by{|k,v| v}
-      end
-    end    
-  end
+    end
+  end    
 end
 
 #.max_by{|k,v| v}
